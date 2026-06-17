2ac98e85d8625c8cc4156d67985d28dacb54f784
BasicallyIAmFox <luxutre12345@gmail.com>
2026-06-17 04:53:52 +0200

    Update theory.js

--- theory.js
+++ theory.js
@@ -1766,7 +1766,7 @@ var createChallengeMenu = () => {
                     borderColor: Color.TRANSPARENT,
                     backgroundColor: Color.TRANSPARENT,
                     content: progress,
-                    margin: new Thickness(0, 0, 0, 5),
+                    margin: new Thickness(0, 0, 0, 2),
                 }),
             ]
         });
